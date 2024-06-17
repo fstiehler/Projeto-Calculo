eff3b28a9ebd52ab39112c14b45fd887db0523d4
Fillipi Stiehler <fillipi.stiehler@evoluinfo.com.br>
2024-06-17 16:38:57 -0300

    fix: primeiro commit

---
+++ projeto-calculadora/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+     <div>
+      <h2>Projeto de Calculadora</h2>
+     </div>
+    </div>
+  );
+}
+
+export default App;
